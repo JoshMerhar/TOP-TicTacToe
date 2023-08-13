aace4450054891c648c2e94fb30048f755a39110
Josh Merhar <122338744+JoshMerhar@users.noreply.github.com>
2023-08-13 16:09:15 -0400

    Create functions to add symbols to gameboard

--- script.js
+++ script.js
@@ -1,30 +1,71 @@
-const gameboardModule = (function() {
-    let gameboard = ["o", "x", "o", "o", "x", "x", "x", "o", "x"];
+const gameboardModule = (() => {
+    let gameboard = ["", "", "", "", "", "", "", "", ""];
 
-    function makeGameboard() {
+    const makeGameboard = () => {
         const theBoard = document.querySelector("#gameboard-container");
         for (let i = 0; i < gameboard.length; i++) {
-            const gameboardSpace = document.createElement("div");
+            const gameboardSpace = document.createElement("button");
+            gameboardSpace.id = i;
+            gameboardSpace.classList.add("gameboard-space");
             gameboardSpace.textContent = gameboard[i];
             theBoard.append(gameboardSpace);
         }
     }
-    return {
-        gameboard: gameboard,
-        makeGameboard: makeGameboard,
-    }; 
+
+    const resetGameboard = () => {
+        const allSpaces = document.querySelectorAll(".gameboard-space");
+        allSpaces.forEach(space => space.remove());
+    }
+    return {gameboard, makeGameboard, resetGameboard}; 
 })();
 
-gameboardModule.makeGameboard();
-let gameboard = gameboardModule.gameboard;
-
 function createPlayer(name, symbol) {
-    return {
-        name: name,
-        symbol: symbol,
-        isTurn: false,
-    }
+    return {name, symbol}
 }
 
-const player1 = createPlayer("Player1", "x");
-const player2 = createPlayer("Player2", "o");
+const player1 = createPlayer("Player1", "X");
+const player2 = createPlayer("Player2", "O");
+
+const gameController = (() => {
+    const gameboard = gameboardModule.gameboard;
+    let round = 0;
+
+    function markSpace(e) {
+        let targetSpace = gameboard[`${e.target.id}`];
+        if (targetSpace === "") {
+            if (round === 0 || round === 2 || round === 4 || round === 6 || round === 8) {
+                targetSpace = player1.symbol;
+                gameboard[`${e.target.id}`] = targetSpace;
+            } else if (round === 1 || round === 3 || round === 5 || round === 7) {
+                targetSpace = player2.symbol;
+                gameboard[`${e.target.id}`] = targetSpace;
+            }
+        } else {
+            return;
+        }
+        round++;
+        playGame();
+    }
+
+    function playGame() {
+        gameboardModule.resetGameboard();
+        gameboardModule.makeGameboard();
+        const spaces = document.querySelectorAll(".gameboard-space");
+        spaces.forEach(space => space.addEventListener("click", markSpace))
+    }
+
+    function newGame() {
+        round = 0;
+        for (let i = 0; i < gameboard.length; i++) {
+            gameboardModule.gameboard[i] = "";
+        }
+        playGame();
+    }
+
+    const newGameButton = document.querySelector("#new-game-button");
+    newGameButton.addEventListener("click", newGame);
+
+    return {playGame};
+})();
+
+gameController.playGame();
